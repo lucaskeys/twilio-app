c7ee51541b67186c31a9f87a3df38465aaf886ce
Ryan Back <ryanpback@gmail.com>
2017-02-23 09:46:36 -0800

    remove profile picture

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile_picture])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
 end
